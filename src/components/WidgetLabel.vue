5dd94b98d3cd3ac1a288adccfac0528f6ea83730
Marjie Hensz <marjie.hensz@gmail.com>
2026-03-04 22:10:51 -0500

    label update

--- src/components/WidgetLabel.vue
+++ src/components/WidgetLabel.vue
@@ -26,12 +26,12 @@ onMounted(() => {
     if (ctx.value) {
        const backgroundImage = new Image();
 
+      backgroundImage.src = '${import.meta.env.BASE_URL}/assets/field.png'; // Set image source
       // Make sure the image is loaded before drawing
       backgroundImage.onload = () => {
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
         // Continue drawing other elements over the background
       };
-      backgroundImage.src = '/public/assets/field.png'; // Set image source
       ctx.value.lineWidth = 2;
       ctx.value.strokeStyle = "#fff";
       ctx.value.lineCap = "round";
